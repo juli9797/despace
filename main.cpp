f83a66105bd03546775f3eb63da3cf744df35873
julian <julian.dangelmaier@gmail.com>
2021-07-21 11:22:37 +0200

    file not found error handling

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <algorithm>
-#include <iostream>
-#include <string>
 #include <filesystem>
-
+#include <iostream>
+#include <ostream>
+#include <string>
 
 int main(int argc, char* argv[])
 {
@@ -20,16 +20,25 @@ int main(int argc, char* argv[])
                       << "\033[0m" << std::endl;
 
         } else {
-            std::cout << "'" << src << "'"
+            std::filesystem::path p_src = std::filesystem::current_path() / src;
+            std::filesystem::path p_dest = std::filesystem::current_path() / dest;
+            try {
+                std::filesystem::rename(src, dest);
+                std::cout << "'" << src << "'"
                       << "\033[1;33m"
                       << "\t->\t"
                       << "\033[0m" << dest << std::endl;
-            //std::string command = "mv '" + src + "' " + dest;
-            //std::system(command.c_str());
 
-            std::filesystem::path p_src = std::filesystem::current_path() / src;
-            std::filesystem::path p_dest = std::filesystem::current_path() / dest;
-            std::filesystem::rename(src,dest); 
+            } catch (std::filesystem::filesystem_error const& e) {
+
+                std::cout << "'" << src << "':" 
+                      << "\033[1;33m"
+                      << "\t->\t"
+                      << "\033[0m" << std::endl;
+
+
+                std::cout << e.code().message() <<" "<< e.path1() << std::endl;
+            }
         }
     }
 }
